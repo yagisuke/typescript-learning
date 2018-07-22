fbb467bb3353d2d884fa0da17bed7e04c8435627
yagisuke <yagisuke.0209@gmail.com>
2018-07-22 15:22:21 +0900

    lesson5

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
-/* lesson2: はじめてのTypeScript */
+/*
+ * lesson2: はじめてのTypeScript
+ */
 // $tsc main.ts // -> create the main.js
 // $node main.js // -> display hello world
 class User {}
 console.log('hello world')
 
-/* lesson3: 静的型付けをしてみよう */
+/*
+ * lesson3: 静的型付けをしてみよう
+ */
 // js は動的型付けで以下のようにしても大丈夫だが、
 // ts は静的型付けなのでエラーとなり、安全なプログラミングが実現できる
 // var a3 = 10
 // a3 = 'hello'
 
-/* lesson4: さまざまな型を使ってみよう */
+/*
+ * lesson4: さまざまな型を使ってみよう
+ */
 var a4: number
 var b4: number = 10
 var c4 = 10 // -> 型推論によりnumber型となる
@@ -24,3 +30,17 @@ d4 = 'hello'
 // array
 var e5: number[]
 e5 = [1, 2, 3]
+
+/*
+ * lesson5: 列挙型を使ってみよう
+ */
+enum Signal {
+  Blue = 0,
+  Yellow = 1,
+  Red = 2
+}
+var result: Signal
+console.log('1: ', 0 === Signal.Blue) // -> true
+console.log('2: ', 'Blue' === Signal[0]) // -> true
+console.log('3: ', Signal.Yellow) // -> 1
+console.log('4: ', Signal[2]) // -> Red

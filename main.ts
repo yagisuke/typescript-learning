9942101e91f94158ffee3bbb0b67cb6dd10a90d1
yagisuke <yagisuke.0209@gmail.com>
2018-07-22 15:04:56 +0900

    lesson3

--- main.ts
+++ main.ts
@@ -3,3 +3,9 @@
 // $node main.js // -> display hello world
 class User {}
 console.log('hello world')
+
+/* lesson3: 静的型付けをしてみよう */
+// js は動的型付けで以下のようにしても大丈夫だが、
+// ts は静的型付けなのでエラーとなり、安全なプログラミングが実現できる
+// var a3 = 10
+// a3 = 'hello'

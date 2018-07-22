8926d971cb734361188a83bd7449c16f8ecf310a
yagisuke <yagisuke.0209@gmail.com>
2018-07-22 15:14:03 +0900

    lesson4

--- main.ts
+++ main.ts
@@ -9,3 +9,18 @@ console.log('hello world')
 // ts は静的型付けなのでエラーとなり、安全なプログラミングが実現できる
 // var a3 = 10
 // a3 = 'hello'
+
+/* lesson4: さまざまな型を使ってみよう */
+var a4: number
+var b4: number = 10
+var c4 = 10 // -> 型推論によりnumber型となる
+
+// any型について
+// any型は、動的な型ではなく、any型となるのがポイント
+var d4
+d4 = 10
+d4 = 'hello'
+
+// array
+var e5: number[]
+e5 = [1, 2, 3]
